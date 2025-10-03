c1bbd14cc44735e856a6568bfd4e1a83dd3a649a
SUHAIL KHAN <143876344+iamsuhailkhan@users.noreply.github.com>
2025-10-03 11:24:39 +0530

    Update Home.jsx
    change api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,7 +74,7 @@ const Home = () => {
 
   // It’s insecure to put API key in client code — better to proxy via your backend.
   const ai = new GoogleGenAI({
-  apiKey: "AIzaSyDcrSsiD5ZwDZ4NhKAJR8OPthkEIoLXhBM",  
+  apiKey: "Add your API Key Here",  
 });
 
 
